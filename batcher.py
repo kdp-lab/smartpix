5e9efc4e09cd1f06791a35f4320d68e961b201e1
Anthony Badea <abadea@login02.af.uchicago.edu>
2023-09-29 10:17:40 -0500

    BIG CHANGE: move to just using raw->h5 rather than raw->csv so changed batcher to h5 loading

--- batcher.py
+++ batcher.py
@@ -5,42 +5,34 @@ batch manager for handling a list of files on input. loads them asynchronously t
 # python imports
 import torch
 import numpy as np
-import pandas as pd
 import argparse
+import h5py
 
-def loadDataFromCSV(xInFileName, yInFileName, nrows=None):
+def loadDataFromH5(inFileName):
+    
+    # load in the data
+    with h5py.File(inFileName) as f:
+        x = np.array(f["data"])
+        y = np.array(f["labels"])
+    
+    # the labels are x-entry, y-entry, z-entry, n_x, n_y, n_z, number_eh_pairs, y-local, pt
+    # taken from https://zenodo.org/record/7331128
+    # other relevant parameters to compute are https://github.com/kdipetri/semiparametric/blob/master/processing/datagen.py#L110C9-L115C95
+    # cotAlpha = y[:,3]/y[:,5] # n_x/n_z
+    cotBeta = y[:,4]/y[:,5] # n_y/n_z
+    # sensor_thickness = 100 #um                                                          
+    # x_midplane = y[:,0] + cotBeta*(sensor_thickness/2 - y[:,2]) # x-entry + cotAlpha*(sensor_thickness/2 - z-entry)
+    # y_midplane = y[:,1] + cotBeta*(sensor_thickness/2 - y[:,2]) # y-entry + cotBeta*(sensor_thickness/2 - z-entry)
 
-    # pick up the data
-    x = pd.read_csv(xInFileName, nrows=nrows).to_numpy().reshape(-1, 20, 21, 13) # np.all(r3d[:,-1] == r2d[:,0]) -> True comparing the 2d and r3d datasets. The r2d is the last time slice of 3d
-    # just do the 2d for now
-    x = x[:,-1].sum(1) # .reshape(x.shape[0], -1) # only last time slice for now
-    # add dummy for the y0 for now
-    x = np.concatenate([x,np.zeros((x.shape[0],1))], -1)
-    # pick up the labels
-    y = pd.read_csv(yInFileName, nrows=nrows) # to get column names list(y.columns): ['x-entry', 'y-entry', 'z-entry', 'n_x', 'n_y', 'n_z', 'number_eh_pairs', 'y-local', 'pt', 'cotAlpha', 'cotBeta', 'y-midplane', 'x-midplane']
-    # y = y['y-midplane'].values # ['x-midplane','y-midplane']
-    # print(y['y-local'].values[:10]) # Question: does y_0 == y-local?
+    # for 1D only the last time slice
+    x = x[:,-1]
 
-    # The models are trained with three output categories:
-    pt = y['pt'].values
-    # 0. positively charged and pT < 200 MeV (0.2 GeV)
-    c0 = (pt > 0) * (abs(pt) < 0.2)
-    # 1. negatively charged and pT < 200 MeV
-    c1 = (pt < 0) * (abs(pt) < 0.2)
-    # 2. pT > 200MeV, both positively and negatively charged.
-    c2 = (abs(pt) > 0.2)
-    # make final label
-    y = np.stack([c0,c1,c2],-1)
-    y = np.where(y==1)[1]
-    # count occurances and print
-    unique, counts = np.unique(y, return_counts=True)
-    print(dict(zip(unique, counts)))
-
-    # convert to tensors
+    # convert to tensor
     x = torch.Tensor(x)
-    y = torch.Tensor(y).long()
+    y = torch.Tensor(cotBeta)
 
-    return x, y
+    return x, y 
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -48,5 +40,5 @@ if __name__ == "__main__":
     parser.add_argument("-y",  "--yInFileName", default=None, help="Input file")
     ops = parser.parse_args()
 
-    x, y = loadDataFromCSV(ops.xInFileName, ops.yInFileName, None)
+    x, y = loadDataFromH5(ops.xInFileName)
     print(x.shape, y.shape)
