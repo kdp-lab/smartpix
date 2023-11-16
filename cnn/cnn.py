953d15630c7b1e3558977c32060461c0f8a73893
Anthony Badea <abadea@login02.af.uchicago.edu>
2023-11-15 18:27:16 -0600

    testing cnn resolutions

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -24,17 +24,20 @@ def print_size_hook(module, input, output):
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2)
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64 * 3 * 5, 128)  # Adjust based on your input size
+        self.fc1 = nn.Linear(64*6*10, 128)  # Adjust based on your input size # 64 * 3 * 5
         self.fc2 = nn.Linear(128, 3)  # Adjust output size based on your task
 
     def forward(self, x):
         x = x.view(-1, 1, 13, 21)  # Add channel dimension for grayscale image
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 3 * 5)  # Adjust based on your input size
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+        x = x.view(x.shape[0], -1) #-1, 64 * 3 * 5)  # Adjust based on your input size
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         # x = x.flatten()
@@ -99,6 +102,7 @@ def train(model, train_dataloader, val_dataloader, checkpoint_dir="checkpoints",
     # callbacks
     callbacks = [
         ModelCheckpoint(monitor="train_loss", dirpath=checkpoint_dir, filename='cp-{epoch:04d}-{step}', every_n_train_steps = 1, save_top_k=20), # 0=no models, -1=all models, N=n models, set save_top_k=-1 to save all checkpoints
+        EarlyStopping(monitor="val_loss_epoch", min_delta=0.001, patience=5, verbose=True, mode='min'),
     ]
 
     # torch lightning trainer
