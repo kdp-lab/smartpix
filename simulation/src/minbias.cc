d8df48603db6d13231a0e2612a7e4f3dbf353cba
Anthony Badea <badeaa3@gmail.com>
2023-12-04 23:29:04 -0600

    first attempt

--- simulation/src/minbias.cc
+++ simulation/src/minbias.cc
@@ -1,13 +1,13 @@
 /*
 
   Authors: 
-  Matias Mantinan
   Anthony Badea
   Jennet Dickinson
   Karri DiPetrillo
 
   Description:
   Script to generate minbias pythia samples
+  Based on pythia main08 example https://pythia.org/latest-manual/examples/main08.html
 
 */
 
@@ -47,26 +47,25 @@ void pbftp(double time_diff, int nprocessed, int ntotal){
 int main(int argc, char* argv[]) {
 
   // check user is inputting correct parameters
-  if(argc != 4){
-    std::cout << "Usage: ./minbias.exe <pythiaCard> <outFileName> <maxEvents>" << std::endl;
+  if(argc != 3){
+    std::cout << "Usage: ./minbias.exe <outFileName> <maxEvents>" << std::endl;
     return 1;
   }
   // read in user parameters
-  std::string pythiaCard = argv[1];
-  std::string outFileName = argv[2];
-  int maxEvents = std::stoi(argv[3]);
+  // std::string pythiaCard = argv[1];
+  std::string outFileName = argv[1];
+  int maxEvents = std::stoi(argv[2]);
 
   // Generator.
   Pythia pythia;
-
-  // Shorthand for the event record in pythia.
-  Event& event = pythia.event;
-
-  // Read in commands from external file.
-  pythia.readFile(pythiaCard.c_str());
-
-  // Initialize.
-  pythia.init();
+  
+  // Shorthand for some public members of pythia (also static ones).
+  Settings& settings = pythia.settings;
+  const Info& info = pythia.info;
+  // number of bins
+  int nBin = 2;
+  // set up pT bins
+  double pTlimitTwo[3] = {0., 20., 100.};
 
   // Create root file and tree
   TFile* f = new TFile(outFileName.c_str(),"RECREATE");
@@ -105,49 +104,82 @@ int main(int argc, char* argv[]) {
   std::chrono::duration<double> elapsed_seconds;
   time_start = std::chrono::system_clock::now();
 
-  for (int iE = 0; iE < maxEvents; ++iE) {
+  // settings
+  // Loop over number of bins, i.e. number of subruns.
+  for (int iBin = 0; iBin < nBin; ++iBin) {
 
-    if(!pythia.next()) continue;
+    // Normally HardQCD, but in two cases nonDiffractive.
+    // Need MPI on in nonDiffractive to get first interaction, but not else.
+    if(iBin == 0){
+      pythia.readString("HardQCD:all = off");
+      pythia.readString("SoftQCD:nonDiffractive = on");
+    }
+    else{
+      pythia.readString("HardQCD:all = on");
+      pythia.readString("SoftQCD:nonDiffractive = off");
+    }
 
-    // progress bar
-    elapsed_seconds = (std::chrono::system_clock::now() - time_start);
-    pbftp(elapsed_seconds.count(), iE, maxEvents);
+    // Mode 5: hardcoded here. Use settings.parm for non-string input.
+    // Hard processes in one step, but pT-weighted.
+    settings.parm("PhaseSpace:pTHatMin", pTlimitTwo[iBin]);
+    settings.parm("PhaseSpace:pTHatMax", pTlimitTwo[iBin + 1]);
+    if (iBin == 1) {
+      pythia.readString("PhaseSpace:bias2Selection = on");
+      pythia.readString("PhaseSpace:bias2SelectionPow = 4.");
+      pythia.readString("PhaseSpace:bias2SelectionRef = 20.");
+    }
 
-    // clear for new event
-    id->clear();
-    status->clear();
-    mass->clear();
-    pt->clear();
-    eta->clear();
-    phi->clear();
-    tau->clear();
-    xDec->clear();
-    yDec->clear();
-    zDec->clear();
-    tDec->clear();
+    // Initialize for LHC at 14 TeV.
+    pythia.readString("Beams:eCM = 14000.");
+    pythia.init();
     
-    // get event level information
-    nParticles = event.size();
+    // Shorthand for the event record in pythia.
+    Event& event = pythia.event;
+
+    for (int iE = 0; iE < maxEvents; ++iE) {
+
+      if(!pythia.next()) continue;
+
+      // progress bar
+      elapsed_seconds = (std::chrono::system_clock::now() - time_start);
+      pbftp(elapsed_seconds.count(), iE, maxEvents);
+
+      // clear for new event
+      id->clear();
+      status->clear();
+      mass->clear();
+      pt->clear();
+      eta->clear();
+      phi->clear();
+      tau->clear();
+      xDec->clear();
+      yDec->clear();
+      zDec->clear();
+      tDec->clear();
+      
+      // get event level information
+      nParticles = event.size();
     
-    // loop over the particles. available properties listed here https://pythia.org/latest-manual/ParticleProperties.html
-    for(int iP=0; iP<nParticles; iP++){
+      // loop over the particles. available properties listed here https://pythia.org/latest-manual/ParticleProperties.html
+      for(int iP=0; iP<nParticles; iP++){
 
-      // save particle information
-      id->push_back(pythia.event[iP].id());
-      status->push_back(pythia.event[iP].status());
-      mass->push_back(pythia.event[iP].m());
-      pt->push_back(pythia.event[iP].pT());
-      eta->push_back(pythia.event[iP].eta());
-      phi->push_back(pythia.event[iP].phi());
-      tau->push_back(pythia.event[iP].tau());
-      xDec->push_back(pythia.event[iP].xDec());
-      yDec->push_back(pythia.event[iP].yDec());
-      zDec->push_back(pythia.event[iP].zDec());
-      tDec->push_back(pythia.event[iP].tDec());
-   }
+	// save particle information
+	id->push_back(pythia.event[iP].id());
+	status->push_back(pythia.event[iP].status());
+	mass->push_back(pythia.event[iP].m());
+	pt->push_back(pythia.event[iP].pT());
+	eta->push_back(pythia.event[iP].eta());
+	phi->push_back(pythia.event[iP].phi());
+	tau->push_back(pythia.event[iP].tau());
+	xDec->push_back(pythia.event[iP].xDec());
+	yDec->push_back(pythia.event[iP].yDec());
+	zDec->push_back(pythia.event[iP].zDec());
+	tDec->push_back(pythia.event[iP].tDec());
+      }
 
-    // fill tree on each particle loop
-    t->Fill();
+      // fill tree on each particle loop
+      t->Fill();
+    }
   } 
 
   // write and cleanup
