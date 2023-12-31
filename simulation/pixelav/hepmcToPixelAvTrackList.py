1c92f57b732d42b71987e2072e247fddf9c134ba
Anthony Badea <badeaa3@gmail.com>
2023-12-31 17:08:55 -0600

    working

--- simulation/pixelav/hepmcToPixelAvTrackList.py
+++ simulation/pixelav/hepmcToPixelAvTrackList.py
@@ -22,35 +22,41 @@ if __name__ == "__main__":
     # pyhepmc.open can read most HepMC formats using auto-detection
     with pyhepmc.open(ops.inFileName) as f:
         # loop over events
-        for event in f:
+        for iF, event in enumerate(f):
             # loop over particles
-            print(len(event.particles))
+            print(f"Event {iF} has {len(event.particles)} particles")
             for particle in event.particles:
                 
                 # # to get the production vertex and position
                 prod_vertex = particle.production_vertex
                 prod_vector = prod_vertex.position
-                
+                prod_R = prod_vector.perp() # [mm]
+
                 # # to get the decay vertex
                 # decay_vertex = particle.end_vertex
                 # decay_vector = decay_vertex.position
                 
                 # decide which particles to keep
-                accept = (particle.status > 9) * (abs(particle.momentum.eta()) != float("inf"))
-                print(particle.id, particle.pid, particle.status, particle.momentum.pt(), particle.momentum.eta(), particle.momentum.phi(), accept)
+                accept = (particle.status == 1) # final state particle
+                accept *= (abs(particle.momentum.eta()) != float("inf")) # non infinite eta
+                accept *= ( (prod_R >= 30) * (prod_R <= 130) ) # within pixel detector ~ 30-130mm
+
                 if not accept:
                     continue
+
+                print(particle.id, particle.pid, particle.status, particle.momentum.pt(), particle.momentum.eta(), particle.momentum.phi(), prod_vector.x, prod_vector.y, accept)
 
                 # track properties
                 cotb = particle.momentum.eta()
                 cota = particle.momentum.phi()
                 p = particle.momentum.p3mod()
                 flp = 0
-                localx = prod_vector[0] # this needs to be particle position at start of pixel detector or only save particles that are produced within epsilon distance of detector
-                localy = prod_vector[1]
+                localx = prod_vector.x # this needs to be particle position at start of pixel detector or only save particles that are produced within epsilon distance of detector
+                localy = prod_vector.y # [mm]
                 pT = particle.momentum.pt()
                 tracks.append([cotb, cota, p, flp, localx, localy, pT])
-            break
+            
+            break # need to understand what to put in track_list between events
 
         # save to file
         np.savetxt(ops.outFileName, tracks, delimiter=' ', fmt="%f")
